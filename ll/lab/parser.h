d5c4160cec14543e5909c693f78af0b9a2198030
Zhan Xinming <zxm@boluo>
2015-01-10 16:34:29 +0800

    save

--- ll/lab/parser.h
+++ ll/lab/parser.h
@@ -1,4 +1,5 @@
 #include <boost/spirit/home/qi.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
 #include <fstream>
 
 namespace lab
@@ -12,25 +13,47 @@ namespace lab
     }
 }
 
-BOOST_FUSION_ADAPT_STRUCT(ast::node, (std::string, name));
+BOOST_FUSION_ADAPT_STRUCT(lab::ast::node, (std::string, name));
 
 namespace lab
 {
     template < class Iterator >
-    struct grammar : boost::spirit::qi::grammar<Iterator, ast::node(), boost::spirit::ascii::space_type>
+    //struct grammar : boost::spirit::qi::grammar<Iterator, ast::node(), boost::spirit::qi::locals<std::string>, boost::spirit::ascii::space_type>
+    struct grammar : boost::spirit::qi::grammar< Iterator >
     {
         grammar()
             : grammar::base_type(top, "lab")
             , top("top")
+            , stmt("stmt")
+            , expr("expr")
         {
             using boost::spirit::qi::int_;
             using boost::spirit::qi::char_;
             using boost::spirit::qi::lit;
+            using boost::spirit::qi::lit;
+            using boost::spirit::qi::lexeme;
+            using boost::spirit::qi::on_error;
+            using boost::spirit::qi::fail;
+            //using boost::spirit::ascii::char_;
+            //using boost::spirit::ascii::string;
+            using namespace qi::labels;
 
-            top %= *char_;
+            top %= *( stmt | expr ) ;
+
+            stmt %= stmt_decl | stmt_ctrl ;
+
+            expr %= expr_call ;
         }
 
-        boost::spirit::qi::rule< Iterator, ast::node(), boost::spirit::ascii::space_type > top;
+        //boost::spirit::qi::rule< Iterator, ast::node(), boost::spirit::qi::locals<std::string>, boost::spirit::ascii::space_type > top;
+        boost::spirit::qi::rule< Iterator > top;
+        boost::spirit::qi::rule< Iterator > stmt;
+        boost::spirit::qi::rule< Iterator > stmt_decl;
+        boost::spirit::qi::rule< Iterator > stmt_ctrl;
+        boost::spirit::qi::rule< Iterator > stmt_ctrl_with;
+        boost::spirit::qi::rule< Iterator > stmt_ctrl_case;
+        boost::spirit::qi::rule< Iterator > expr;
+        boost::spirit::qi::rule< Iterator > expr_call;
     };
 
     ast::node parse_file(const std::string & filename)
