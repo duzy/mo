d8636a5116939b0fdf2801dc41ee122322dc0a9b
Duzy Chan <code@duzy.info>
2015-01-11 22:29:41 +0800

    save

--- ll/lab/parser.h
+++ ll/lab/parser.h
@@ -1,5 +1,13 @@
-#include <boost/spirit/home/qi.hpp>
+//#include <boost/config/warning_disable.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix_core.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/spirit/include/phoenix_fusion.hpp>
+#include <boost/spirit/include/phoenix_stl.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
+//#include <boost/variant/recursive_variant.hpp>
+//#include <boost/foreach.hpp>
+#include <iostream>
 #include <fstream>
 
 namespace lab
@@ -18,42 +26,85 @@ BOOST_FUSION_ADAPT_STRUCT(lab::ast::node, (std::string, name));
 namespace lab
 {
     template < class Iterator >
-    //struct grammar : boost::spirit::qi::grammar<Iterator, ast::node(), boost::spirit::qi::locals<std::string>, boost::spirit::ascii::space_type>
-    struct grammar : boost::spirit::qi::grammar< Iterator >
+    struct grammar : boost::spirit::qi::grammar<Iterator, ast::node(), boost::spirit::qi::locals<std::string>, boost::spirit::ascii::space_type>
     {
-        grammar()
-            : grammar::base_type(top, "lab")
-            , top("top")
-            , stmt("stmt")
-            , expr("expr")
+        grammar() : grammar::base_type(top, "lab")
         {
             using boost::spirit::qi::int_;
             using boost::spirit::qi::char_;
             using boost::spirit::qi::lit;
-            using boost::spirit::qi::lit;
+            using boost::spirit::qi::alpha;
+            using boost::spirit::qi::alnum;
             using boost::spirit::qi::lexeme;
             using boost::spirit::qi::on_error;
             using boost::spirit::qi::fail;
+            using boost::spirit::lazy;
             //using boost::spirit::ascii::char_;
             //using boost::spirit::ascii::string;
-            using namespace qi::labels;
+            //using namespace boost::spirit::qi::labels;
+
+            using boost::phoenix::val;
 
             top %= *( stmt | expr ) ;
 
-            stmt %= stmt_decl | stmt_ctrl ;
+            stmt = decl | ctrl ;
 
-            expr %= expr_call ;
+            ctrl = with | ctrl_case ;
+
+            with = lit("with") > with_X ;
+
+            with_X
+                = with_newnode
+                | with_attribute
+                | with_variable
+                ;
+
+            with_newnode
+                = lit("{")
+                > lit("}")
+                ;
+
+            with_attribute = lexeme
+                [ ":" >> name ]
+                ;
+
+            ctrl_case = lit("case") ;
+
+            comment = "#" ;
+
+            prop = ":" >> name >> -( "(" >> ( expr % "," ) >> ")" );
+
+            name = lexeme[ alpha >> *alnum ];
+
+            expr = expr_call ;
+
+
+
+            top         .name("top");
+            stmt        .name("stmt");
+            expr        .name("expr");
+
+            on_error<fail>
+            (
+             top, std::cout << val("error") << std::endl
+            );
         }
 
-        //boost::spirit::qi::rule< Iterator, ast::node(), boost::spirit::qi::locals<std::string>, boost::spirit::ascii::space_type > top;
-        boost::spirit::qi::rule< Iterator > top;
+        boost::spirit::qi::rule< Iterator, ast::node(), boost::spirit::qi::locals<std::string>, boost::spirit::ascii::space_type > top;
         boost::spirit::qi::rule< Iterator > stmt;
-        boost::spirit::qi::rule< Iterator > stmt_decl;
-        boost::spirit::qi::rule< Iterator > stmt_ctrl;
-        boost::spirit::qi::rule< Iterator > stmt_ctrl_with;
-        boost::spirit::qi::rule< Iterator > stmt_ctrl_case;
+        boost::spirit::qi::rule< Iterator > decl;
+        boost::spirit::qi::rule< Iterator > ctrl;
+        boost::spirit::qi::rule< Iterator > with;
+        boost::spirit::qi::rule< Iterator > with_X;
+        boost::spirit::qi::rule< Iterator > with_newnode;
+        boost::spirit::qi::rule< Iterator > with_attribute;
+        boost::spirit::qi::rule< Iterator > with_variable;
+        boost::spirit::qi::rule< Iterator > ctrl_case;
         boost::spirit::qi::rule< Iterator > expr;
         boost::spirit::qi::rule< Iterator > expr_call;
+        boost::spirit::qi::rule< Iterator > comment;
+        boost::spirit::qi::rule< Iterator > prop;
+        boost::spirit::qi::rule< Iterator > name;
     };
 
     ast::node parse_file(const std::string & filename)
